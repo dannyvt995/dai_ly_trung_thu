e8ae9984d28dd23c3344c1736939a27729cf124f
nhatvy1 <vyhuynhak123@gmail.com>
2024-08-02 00:10:51 +0700

    update increase, decrease

--- src/components/commons/AddProduct.tsx
+++ src/components/commons/AddProduct.tsx
@@ -15,14 +15,39 @@ const AddProduct = ({ item }: any) => {
     addProduct(item, quantity, weight)
   }
 
-  useEffect(()=> {
+  const handleOnChange = (e: any)=> {
+    setWeight(e.target.value)
+    setQuantity(1)
+  }
+
+  const handleQuantity = (str: 'increase' | 'decrease') => {
+    switch (str) {
+      case 'increase':
+        if(quantity >= 1) {
+          setQuantity((prev) => prev + 1)
+        }
+        break
+      case 'decrease':
+        if(quantity > 1) {
+          setQuantity((prev) => prev - 1)
+        }
+        break
+      default:
+        return
+    }
+  }
+
+  useEffect(() => {
     setWeight(Object.keys(item?.trongluong)[0])
     setQuantity(1)
   }, [item])
 
   return (
     <div className='flex items-center space-x-2'>
-      <button className='size-[30px] flex items-center justify-center border'>
+      <button
+        className='size-[30px] flex items-center justify-center border'
+        onClick={() => handleQuantity('decrease')}
+      >
         <RiSubtractFill />
       </button>
       <input
@@ -32,12 +57,15 @@ const AddProduct = ({ item }: any) => {
         min={1}
         className='w-12 h-[30px] outline-none text-center border'
       />
-      <button className='size-[30px] flex items-center justify-center border'>
+      <button
+        className='size-[30px] flex items-center justify-center border'
+        onClick={() => handleQuantity('increase')}
+      >
         <MdOutlineAdd size={18} />
       </button>
       <select
         className='border p-1'
-        onChange={(e) => setWeight(e.target.value)}
+        onChange={handleOnChange}
       >
         {item.trongluong &&
           Object.keys(item.trongluong).map((key) => {
